f55c386699ffb5a50025087f1b222f0e395b45ad
root <smadala98@gmail.com>
2020-04-09 01:45:28 -0700

    Part 1-3 finished, but runs slow on Visual Studio Code terminal, hopefully better on SeasNet.

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -1,14 +1,14 @@
 package mapreduce
 
-import "container/list"
-import "fmt"
-
+import (
+	"container/list"
+	"fmt"
+)
 
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
 }
-
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
@@ -28,7 +28,82 @@ func (mr *MapReduce) KillWorkers() *list.List {
 	return l
 }
 
+func RunParallelJob(worker string, operation JobType, jobNum int, numOtherPhase int, c chan string) {
+	args := &DoJobArgs{}
+	args.Operation = operation
+	args.JobNumber = jobNum
+	args.NumOtherPhase = numOtherPhase
+	args.File = "824-mrinput.txt"
+	reply := &DoJobReply{}
+	//fmt.Printf("Worker %v performing %v %v\n", worker, args.Operation, args.JobNumber)
+	if !call(worker, "Worker.DoJob", args, &reply) {
+		worker = <-c
+		go RunParallelJob(worker, operation, jobNum, numOtherPhase, c)
+	}
+	//fmt.Printf("Worker %v completed %v %v\n", worker, args.Operation, args.JobNumber)
+	c <- worker
+}
+
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+	fmt.Println("Running Master")
+
+	mr.Workers = make(map[string]*WorkerInfo)
+
+	workerChannel := make(chan string)
+
+	mapWorkerCount := 0
+	for {
+		if mapWorkerCount == mr.nMap {
+			break
+		}
+		select {
+		case worker := <-mr.registerChannel:
+			mr.Workers[worker] = &WorkerInfo{worker}
+			go RunParallelJob(worker, "Map", mapWorkerCount, mr.nReduce, workerChannel)
+			mapWorkerCount++
+		case oldWorker := <-workerChannel:
+			go RunParallelJob(oldWorker, "Map", mapWorkerCount, mr.nReduce, workerChannel)
+			mapWorkerCount++
+			//default:
+			//fmt.Println("No workers available for map")
+		}
+	}
+
+	for i := 0; i < len(mr.Workers); i++ {
+		<-workerChannel
+		//worker := <-workerChannel
+		//fmt.Printf("Worker %v Finished Map\n", worker)
+	}
+
+	workerIndex := 0
+	for worker := range mr.Workers {
+		go RunParallelJob(worker, "Reduce", workerIndex, mr.nMap, workerChannel)
+		workerIndex++
+	}
+
+	reduceWorkerCount := 0
+	for {
+		if reduceWorkerCount == mr.nReduce {
+			break
+		}
+		select {
+		case worker := <-mr.registerChannel:
+			mr.Workers[worker] = &WorkerInfo{worker}
+			go RunParallelJob(worker, "Reduce", reduceWorkerCount, mr.nMap, workerChannel)
+			reduceWorkerCount++
+		case oldWorker := <-workerChannel:
+			go RunParallelJob(oldWorker, "Reduce", reduceWorkerCount, mr.nMap, workerChannel)
+			reduceWorkerCount++
+			//default:
+			//fmt.Println("No workers available for reduce")
+		}
+	}
+
+	for i := 0; i < len(mr.Workers); i++ {
+		<-workerChannel
+		//worker := <-workerChannel
+		//fmt.Printf("Worker %v Finished Reduce\n", worker)
+	}
 	return mr.KillWorkers()
 }
